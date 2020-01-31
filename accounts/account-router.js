5344dc673e0a6388733ad24b8c0168597bf44b5d
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-30 20:19:08 -0800

    GET and GET by ID Done

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -4,4 +4,26 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        const accounts  = await db("accounts");
+        res.json(accounts);
+    } catch (err) {
+        res.status(500).json({ Message: "Failed to get Accounts"})
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const accounts = await db('accounts').where('id', id);
+        res.json(accounts);
+    } catch (err){
+        res.status(500).json({
+            Message: "This id does not exist"
+        })
+    }
+})
+
+
 module.exports = router;
