5211292c647b05e4bcf84d3400958eff4ab2d5a4
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-30 20:35:55 -0800

    ALL CRUD done MVP

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -25,5 +25,34 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+router.post('/add', async (req, res) => {
+    const bodyData = req.body;
+    try{
+        const accounts = await db('accounts').insert(bodyData);
+        res.status(201).json({ Message: 'Account Successfully Created', accounts});
+    }catch(err){
+        res.status(500).json({message: 'failed to post account'})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const accounts = await db('accounts').where('id', id).update(req.body);
+        res.status(200).json({ Message: 'Account Successfully Updated', accounts});
+    }catch(err){
+        res.status(404).json({message: 'failed to find id'})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const accounts = await db('accounts').where('id', id).delete(id);
+        res.status(204).json({ Message: 'Account Successfully Deleted', accounts});
+    }catch(err){
+        res.status(404).json({message: 'failed to find id'})
+    }
+})
 
 module.exports = router;
